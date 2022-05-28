cacb54a075a19585cb5cbf9ac933f9912cfc0ee9
Randy <zhangzp9970@outlook.com>
2022-05-28 19:24:11 +0800

    ok

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# 表白信封使用教程
+# WebLetter
 
-## 本地调试
+A letter using web technologies, based on george-chou's work: [george-chou/eLuvLetter: Web luv letter (github.com)](https://github.com/george-chou/eLuvLetter)
 
-### 安装 Git
-教程：<a href="https://www.bilibili.com/video/BV1BE411g7SV" target="_blank">bilibili.com/video/BV1BE411g7SV</a>
+## Highlights
 
-下载：<a href="https://git-scm.com/downloads" target="_blank">git-scm.com/downloads</a>
+* add a scroll bar in the letter, make it available for large contents
+* update typed.js and remove jquery (use DOM instead)
+* removed music button and autoplay music by default
+* removed redundant css sheets
+* chage the backgrounds
 
-### 安装 XAMPP
-教程：<a href="https://www.bilibili.com/video/BV1e7411u7qY" target="_blank">bilibili.com/video/BV1e7411u7qY</a>
+**NOTE: THE PROPERTY FILES SUCH AS THE IMAGES AND MUSIC ARE REMOVED, YOU SHOULD ADD THEM MANUALLY BEFORE USING THIS PROJECT!**
 
-下载：<a href="https://www.apachefriends.org/download.html" target="_blank">apachefriends.org/download.html</a>
+## License
 
-### 本地部署
-首先注册一个 GitHub 账号并 fork 一份本仓库到你自己的账号
-<br>
-在 cmd 中切换到 XAMPP 的 htdocs 目录下，克隆仓库：
-```
-git clone https://github.com/你的github用户名/eLuvLetter.git
-```
+BSD 2-Clause License
 
-进入 ./eLuvLetter/font/ 目录下打开 content.json，按如下说明自定义配置：
+Copyright (c) 2022, Zeping Zhang All rights reserved.
 
-```
-{
-    "to"        : "信封收件人",
-    "from"      : "信纸落款",
-    "recipient" : "信纸收件人",
-    "bgm"       : "BGM URL",
-    "text"      : "信纸内容"
-}
-```
-
-其中demo里符号 `^n` 的含义是打字机停顿 n 毫秒，
-
-BGM URL可以是 demo 代码中带的默认 mp3 目录，也可以是网络 mp3 直链(注：最好不用 API 获取 mp3，可能会被跨域封锁)
-<br>
-如果使用默认目录，可将自己的 mp3 重命名成 bgm.mp3 覆盖掉 ./eLuvLetter/bgm 下的 bgm.mp3 实现自定义修改 BGM
-
-### 本地展示
-在 XAMPP 上打开 Apache，进入 <a href="http://localhost/acg-album" target="_blank">localhost/acg-album</a>
-<br>
-建议使用本地展示调试完毕后再进行下一步
-
-## 部署到 Vercel 及广域网展示
-首先将修改过的本地代码同步到你的 GitHub 账号上：
-
-命令行进入 ./eLuvLetter 后
-```
-git add .
-git commit -a
-输入 :wq! 回车
-git push
-```
-
-再进入<a href="https://vercel.com/login" target="_blank">vercel 官网</a>
-
-使用你自己的 GitHub 账号授权登录，点击 `+ New Project`，
-
-在 Import Git Repository 的搜索框中键入关键字 eLuvLetter 找到之前 fork 的那个仓库，
-
-点击 `Import`，修改 Configure Project 下的 PROJECT NAME 
-
-保证 PROJECT NAME 不与别人冲突后点击 `Deploy`
-
-部署完成后，可在广域网中访问 `https://%PROJECT NAME%.vercel.app` 查看网页。(其中 %PROJECT NAME% 就是刚刚 Configure Project 下修改的 PROJECT NAME)
-
-## 未来工作
-目前信纸内容有字数限制，未来有余力可在信纸上添加滚轴实现字数扩展，也欢迎各位大神能在此基础上把这些问题优化掉来 merge。
+The letter.psd is distributed under [Attribution-NonCommercial-ShareAlike 4.0 International](http://creativecommons.org/licenses/by-nc-sa/4.0/) license.
--- js/letter.js
+++ js/letter.js
@@ -1,135 +1,41 @@
-let envelope_opened = false;
+document.getElementById("open").onclick = function () {
+	document.getElementById("data").innerHTML = "";
+	var changeline = "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+	var p1 = 'PARAGRAPH';
+	var p2 = 'PARAGRAPH';
+	var p3 = 'PARAGRAPH';
+	var p4 = 'PARAGRAPH';
+	var sign = '<br><div align="right">NAME</div>'
 
-let content = {
-	to: "",
-	from: "",
-	recipient: "",
-	text: "",
-	sign: 0
+	var options = {
+		strings: [
+			'',
+			'NAME: ' + changeline + p1 + changeline + p2 + changeline + p3 + changeline + p4 + sign
+		],
+		typeSpeed: 100,
+		backSpeed: 100,
+	};
+	var typed = new Typed('#data', options);
 };
 
-$("#open").click(function () {
+document.getElementById("about").onclick = function () {
+	document.getElementById("data").innerHTML = "";
+	var options = {
+		strings: [
+			'',
+			'`<div align="center"><b>About</b></div>`<br>^100<b>WebLetter 1.0.0</b><br>Copyright &copy; 2022 Zeping Zhang<br>`<b>Licence</b>`<br>BSD 2-Clause License<br><b>Third party libraries</b><br>^100 `<a class="external-link" href="https://github.com/george-chou/eLuvLetter">george-chou/eLuvLetter</a>`'
+		],
+		typeSpeed: 50,
+		backSpeed: 50
+	};
 
-	if (!envelope_opened) {
+	var typed = new Typed('#data', options);
+};
 
-		new Typed('.letter', {
-			strings: [
-				"^1000Dear&nbsp;&nbsp;" + content.to,
-				content.recipient + "<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
-				content.text + "<br><br><p style='float:right; display:block; width:" +
-				content.sign + "px;'>^1000" + content.from + "</p>"
-			],
-			typeSpeed: 100,
-			backSpeed: 50
-		});
-
-		$('#open').find("span").eq(0).css('background-position', "0 -150px");
-
-		envelope_opened = true;
-
-		let player = document.getElementById('music');
-		if (player.paused) {
-			player.play();
-			$('#music_btn').css("opacity", "1");
-			$('#music_btn').css("display", "block");
-		}
-	}
-
-});
-
-function playPause() {
-	let player = document.getElementById('music');
-	let play_btn = $('#music_btn');
-
-	if (player.paused) {
-		player.play();
-		play_btn.css("opacity", "1");
-	}
-	else {
-		player.pause();
-		play_btn.css("opacity", "0.2");
-	}
-}
-
-
-function toBase64(audioURL) {
-	window.URL = window.URL || window.webkitURL;
-	let xhr = new XMLHttpRequest();
-	xhr.responseType = "blob";
-	xhr.open("GET", audioURL, true);
-	xhr.onload = function () {
-		if (this.status == 200) {
-			let reader = new FileReader();
-			reader.readAsDataURL(this.response);
-			reader.onload = function () {
-				$('#music').attr('src', reader.result);
-				$('#envelope').fadeIn('slow');
-				$('.heart').fadeOut('fast');
-				let currentUrl = window.location.href;
-				let firstIndex = currentUrl.indexOf("#");
-				if (firstIndex <= 0) window.location.href = currentUrl + "#contact";
-			};
-
-		}
-	}
-	xhr.send(null);
-}
-
-
-window.onresize = function () {
-	let cherry_container = $('#jsi-cherry-container');
-	let canvas = cherry_container.find('canvas').eq(0);
-	canvas.height(cherry_container.height());
-	canvas.width(cherry_container.width());
-	// Do scaling for sakura background when the window is resized
-	loadingPage();
-}
-
-function loadingPage() {
-	let heart_div = $('.heart');
-	let heart_parent = heart_div.parent();
-	let page_width = heart_parent.width();
-	let page_height = heart_parent.height();
-	let heart_width = heart_div.width();
-	let heart_height = heart_div.height();
-	heart_div.css('top', (page_height - heart_height) / 2);
-	heart_div.css('left', (page_width - heart_width) / 2);
-}
 
 window.onload = function () {
 
-	loadingPage();
-
-	$.ajaxSettings.async = false;
-	$.getJSON("./font/content.json", function (result) {
-		content.to = result.to;
-		content.from = result.from;
-		content.recipient = result.recipient;
-		content.text = result.text;
-		content.sign = (2 + content.from.length) * 20;
-		$('#recipient').append(content.to);
-		toBase64(result.bgm);
-	});
-
-	document.addEventListener('touchstart', function (event) {
-		if (event.touches.length > 1) event.preventDefault();
-	});
-
-	let lastTouchEnd = 0;
-
-	document.addEventListener('touchend', function (event) {
-
-		let now = (new Date()).getTime();
-		if (now - lastTouchEnd <= 300) event.preventDefault();
-		lastTouchEnd = now;
-
-	}, false);
-
-	document.addEventListener('gesturestart', function (event) {
-		event.preventDefault();
-	});
-
-	$('body').css('opacity', '1');
-	$('#jsi-cherry-container').css('z-index', '-99');
-
+	let currentUrl = window.location.href;
+	let firstIndex = currentUrl.indexOf("#");
+	if (firstIndex <= 0) window.location.href = currentUrl + "#contact";
 }
