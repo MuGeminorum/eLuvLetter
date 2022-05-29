43fdadaaa5357e84d08f6213e576354bc4bc5fec
George <20459298+george-chou@users.noreply.github.com>
2022-05-29 19:01:08 +0800

    fix warning in style.css & refine some css and js

--- js/sakura.js
+++ js/sakura.js
@@ -1,64 +1,63 @@
 var RENDERER = {
-	INIT_CHERRY_BLOSSOM_COUNT : 9,	// default : 30
-	MAX_ADDING_INTERVAL : 30,		// default : 10
-	
-	init : function(){
+	INIT_CHERRY_BLOSSOM_COUNT: 9,	// default : 30
+	MAX_ADDING_INTERVAL: 30,		// default : 10
+
+	init: function () {
 		this.setParameters();
 		this.reconstructMethods();
 		this.createCherries();
 		this.render();
 	},
-	setParameters : function(){
+	setParameters: function () {
 		this.$container = $('#jsi-cherry-container');
 		this.width = this.$container.width();
 		this.height = this.$container.height();
-		this.context = $('<canvas />').attr({width : this.width, height : this.height, position : 'absolute', top : 0}).appendTo(this.$container).get(0).getContext('2d');
+		this.context = $('<canvas />').attr({ width: this.width, height: this.height, position: 'absolute', top: 0 }).appendTo(this.$container).get(0).getContext('2d');
 		this.cherries = [];
 		this.maxAddingInterval = Math.round(this.MAX_ADDING_INTERVAL * 1000 / this.width);
 		this.addingInterval = this.maxAddingInterval;
 	},
-	reconstructMethods : function(){
+	reconstructMethods: function () {
 		this.render = this.render.bind(this);
 	},
-	createCherries : function(){
-		for(var i = 0, length = Math.round(this.INIT_CHERRY_BLOSSOM_COUNT * this.width / 1000); i < length; i++){
+	createCherries: function () {
+		for (var i = 0, length = Math.round(this.INIT_CHERRY_BLOSSOM_COUNT * this.width / 1000); i < length; i++) {
 			this.cherries.push(new CHERRY_BLOSSOM(this, true));
 		}
 	},
-	render : function(){
+	render: function () {
 		requestAnimationFrame(this.render);
 		this.context.clearRect(0, 0, this.width, this.height);
-		
-		this.cherries.sort(function(cherry1, cherry2){
+
+		this.cherries.sort(function (cherry1, cherry2) {
 			return cherry1.z - cherry2.z;
 		});
-		for(var i = this.cherries.length - 1; i >= 0; i--){
-			if(!this.cherries[i].render(this.context)){
+		for (var i = this.cherries.length - 1; i >= 0; i--) {
+			if (!this.cherries[i].render(this.context)) {
 				this.cherries.splice(i, 1);
 			}
 		}
-		if(--this.addingInterval == 0){
+		if (--this.addingInterval == 0) {
 			this.addingInterval = this.maxAddingInterval;
 			this.cherries.push(new CHERRY_BLOSSOM(this, false));
 		}
 	}
 };
 
-var CHERRY_BLOSSOM = function(renderer, isRandom)
-{
+var CHERRY_BLOSSOM = function (renderer, isRandom) {
 	this.renderer = renderer;
 	this.init(isRandom);
 };
 
 CHERRY_BLOSSOM.prototype = {
-	FOCUS_POSITION : 300,
-	FAR_LIMIT : 600,
-	MAX_RIPPLE_COUNT : 10,	// default : 100
-	RIPPLE_RADIUS : 10,		// default : 100
-	SURFACE_RATE : 0.5,
-	SINK_OFFSET : 20,
-	
-	init : function(isRandom){
+	FOCUS_POSITION: 300,
+	FAR_LIMIT: 600,
+	MAX_RIPPLE_COUNT: 10,	// default : 100
+	RIPPLE_RADIUS: 10,		// default : 100
+	SURFACE_RATE: 0.5,
+	SINK_OFFSET: 20,
+
+	init: function (isRandom) {
 		this.x = this.getRandomValue(-this.renderer.width, this.renderer.width);
 		this.y = isRandom ? this.getRandomValue(0, this.renderer.height) : this.renderer.height * 1.5;
 		this.z = this.getRandomValue(0, this.FAR_LIMIT);
@@ -72,11 +71,11 @@ CHERRY_BLOSSOM.prototype = {
 		this.endTheta = false;
 		this.endPhi = false;
 		this.rippleCount = 0;
-		
+
 		var axis = this.getAxis(),
 			theta = this.theta + Math.ceil(-(this.y + this.renderer.height * this.SURFACE_RATE) / this.vy) * Math.PI / 500;
 		theta %= Math.PI * 2;
-		
+
 		this.offsetY = 40 * ((theta <= Math.PI / 2 || theta >= Math.PI * 3 / 2) ? -1 : 1);
 		this.thresholdY = this.renderer.height / 2 + this.renderer.height * this.SURFACE_RATE * axis.rate;
 		this.entityColor = this.renderer.context.createRadialGradient(0, 40, 0, 0, 40, 80);
@@ -88,33 +87,33 @@ CHERRY_BLOSSOM.prototype = {
 		this.shadowColor.addColorStop(0.05, 'hsl(330, 40%,' + 30 * (0.3 + axis.rate) + '%)');
 		this.shadowColor.addColorStop(1, 'hsl(330, 20%, ' + 40 * (0.3 + axis.rate) + '%)');
 	},
-	getRandomValue : function(min, max){
+	getRandomValue: function (min, max) {
 		return min + (max - min) * Math.random();
 	},
-	getAxis : function(){
+	getAxis: function () {
 		var rate = this.FOCUS_POSITION / (this.z + this.FOCUS_POSITION),
 			x = this.renderer.width / 2 + this.x * rate,
 			y = this.renderer.height / 2 - this.y * rate;
-		return {rate : rate, x : x, y : y};
+		return { rate: rate, x: x, y: y };
 	},
-	renderCherry : function(context, axis){
+	renderCherry: function (context, axis) {
 		context.beginPath();
 		context.moveTo(0, 40);
 		context.bezierCurveTo(-60, 20, -10, -60, 0, -20);
 		context.bezierCurveTo(10, -60, 60, 20, 0, 40);
 		context.fill();
-		
-		for(var i = -4; i < 4; i++){
+
+		for (var i = -4; i < 4; i++) {
 			context.beginPath();
 			context.moveTo(0, 40);
 			context.quadraticCurveTo(i * 12, 10, i * 4, -24 + Math.abs(i) * 2);
 			context.stroke();
 		}
 	},
-	render : function(context){
+	render: function (context) {
 		var axis = this.getAxis();
-		
-		if(axis.y == this.thresholdY && this.rippleCount < this.MAX_RIPPLE_COUNT){
+
+		if (axis.y == this.thresholdY && this.rippleCount < this.MAX_RIPPLE_COUNT) {
 			context.save();
 			context.lineWidth = 2;
 			context.strokeStyle = 'hsla(0, 0%, 100%, ' + (this.MAX_RIPPLE_COUNT - this.rippleCount) / this.MAX_RIPPLE_COUNT + ')';
@@ -126,8 +125,8 @@ CHERRY_BLOSSOM.prototype = {
 			context.restore();
 			this.rippleCount++;
 		}
-		if(axis.y < this.thresholdY || (!this.endTheta || !this.endPhi)){
-			if(this.y <= 0){
+		if (axis.y < this.thresholdY || (!this.endTheta || !this.endPhi)) {
+			if (this.y <= 0) {
 				this.opacity = Math.min(this.opacity + 0.01, 1);
 			}
 			context.save();
@@ -150,20 +149,20 @@ CHERRY_BLOSSOM.prototype = {
 		context.translate(0, this.offsetY);
 		this.renderCherry(context, axis);
 		context.restore();
-		
-		if(this.y <= -this.renderer.height / 4){
-			if(!this.endTheta){
-				for(var theta = Math.PI / 2, end = Math.PI * 3 / 2; theta <= end; theta += Math.PI){
-					if(this.theta < theta && this.theta + Math.PI / 200 > theta){
+
+		if (this.y <= -this.renderer.height / 4) {
+			if (!this.endTheta) {
+				for (var theta = Math.PI / 2, end = Math.PI * 3 / 2; theta <= end; theta += Math.PI) {
+					if (this.theta < theta && this.theta + Math.PI / 200 > theta) {
 						this.theta = theta;
 						this.endTheta = true;
 						break;
 					}
 				}
 			}
-			if(!this.endPhi){
-				for(var phi = Math.PI / 8, end = Math.PI * 7 / 8; phi <= end; phi += Math.PI * 3 / 4){
-					if(this.phi < phi && this.phi + Math.PI / 200 > phi){
+			if (!this.endPhi) {
+				for (var phi = Math.PI / 8, end = Math.PI * 7 / 8; phi <= end; phi += Math.PI * 3 / 4) {
+					if (this.phi < phi && this.phi + Math.PI / 200 > phi) {
 						this.phi = Math.PI / 8;
 						this.endPhi = true;
 						break;
@@ -171,27 +170,27 @@ CHERRY_BLOSSOM.prototype = {
 				}
 			}
 		}
-		if(!this.endTheta){
-			if(axis.y == this.thresholdY){
+		if (!this.endTheta) {
+			if (axis.y == this.thresholdY) {
 				this.theta += Math.PI / 200 * ((this.theta < Math.PI / 2 || (this.theta >= Math.PI && this.theta < Math.PI * 3 / 2)) ? 1 : -1);
-			}else{
+			} else {
 				this.theta += Math.PI / 500;
 			}
 			this.theta %= Math.PI * 2;
 		}
-		if(this.endPhi){
-			if(this.rippleCount == this.MAX_RIPPLE_COUNT){
+		if (this.endPhi) {
+			if (this.rippleCount == this.MAX_RIPPLE_COUNT) {
 				this.psi += this.dpsi;
 				this.psi %= Math.PI * 2;
 			}
-		}else{
+		} else {
 			this.phi += Math.PI / ((axis.y == this.thresholdY) ? 200 : 500);
 			this.phi %= Math.PI;
 		}
-		if(this.y <= -this.renderer.height * this.SURFACE_RATE){
+		if (this.y <= -this.renderer.height * this.SURFACE_RATE) {
 			this.x += 2;
 			this.y = -this.renderer.height * this.SURFACE_RATE;
-		}else{
+		} else {
 			this.x += this.vx;
 			this.y += this.vy;
 		}
@@ -199,4 +198,4 @@ CHERRY_BLOSSOM.prototype = {
 	}
 };
 
-$(function(){ RENDERER.init(); });
+$(function () { RENDERER.init(); });
