ceea911f0be19dcf9389eb2f6afce86b175f17ae
admin <admin@users.noreply.github.com>
2024-08-08 15:08:57 +0800

    constrain pan-y

--- js/main.js
+++ js/main.js
@@ -37,14 +37,6 @@ window.onload = function () {
         let firstIndex = currentUrl.indexOf("#");
         if (firstIndex <= 0) window.location.href = currentUrl + "#contact";
     });
-    document.addEventListener('touchstart', function (event) { if (event.touches.length > 1) event.preventDefault(); });
-    let lastTouchEnd = 0;
-    document.addEventListener('touchend', function (event) {
-        let now = (new Date()).getTime();
-        if (now - lastTouchEnd <= 300) event.preventDefault();
-        lastTouchEnd = now;
-    }, false);
-    document.addEventListener('gesturestart', function (event) { event.preventDefault(); });
     let contact = $('#contact');
     let mtop = (window.innerHeight - contact.height()) * 0.5;
     contact.css('margin-top', mtop + 'px');
